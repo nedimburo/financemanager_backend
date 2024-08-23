5f95530b76a2a3b5db5992c9627fe250d714b7d2
nedimburo <nedimb9402@gmail.com>
2024-08-23 21:24:05 +0200

    Initial files commit

---
+++ src/main/java/org/finance/financemanager/FinancemanagerBackendApplication.java
@@ -0,0 +1,13 @@
+package org.finance.financemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancemanagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancemanagerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/finance/financemanager/FinancemanagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.finance.financemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancemanagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
